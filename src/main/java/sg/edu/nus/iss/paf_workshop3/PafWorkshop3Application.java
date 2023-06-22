7d9ee366833c9259e83c5f01376000f01be1cc9d
driftlog <driftlogged@gmail.com>
2023-06-22 14:07:13 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_workshop3/PafWorkshop3Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_workshop3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafWorkshop3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafWorkshop3Application.class, args);
+	}
+
+}
